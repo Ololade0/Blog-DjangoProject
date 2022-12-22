b3e56dbffcb12a41a03caa52d1508b215d919e2b
Ololade0 <adesuyiololade@gmail.com>
2022-12-22 15:26:59 +0100

    Store front

--- store/urls.py
+++ store/urls.py
@@ -5,9 +5,9 @@ from rest_framework_nested import routers
 
 # app_name = "store"
 router = routers.DefaultRouter()
-router.register('products', views.ProductViewSet)
+router.register('products', views.ProductViewSet,basename='products')
 pprint(router.urls)
-product_router =routers.NestedDefaultRouter(router,'products', lookup= 'product_pk')
+product_router = routers.NestedDefaultRouter(router,'products', lookup= 'product_pk')
 product_router.register('reviews', views.ReviewSet, basename='product-reviews')
 
 urlpatterns = router.urls + product_router.urls
